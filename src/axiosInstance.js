7a19cce8e404043ac86cd8ae9f834aaab18ed21e
Derek He <dhe@concertict.com>
2018-03-26 01:42:40 +1100

    change port on client to 5000

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 var instance = axios.create({
-    baseURL : 'http://localhost:3000',
+    baseURL : 'http://localhost:5000',
     proxy: 'http://localhost:4000'
     }
 );
